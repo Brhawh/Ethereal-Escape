b80d438d02d59c39076fb558f84db197d4fa7fea
James Morrison <jamesmorrison3@live.co.uk>
2020-10-23 21:52:39 +0100

    Enemy moves towards mouse with configurable speed

---
+++ Enemy.gd
@@ -0,0 +1,35 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+export (int) var health = 10
+export (float) var speed = 0.01
+export (NodePath) var nav2d
+
+var path = []
+var path_points = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var mouse_pos = get_viewport().get_mouse_position()
+	path = get_node(nav2d).get_simple_path(global_position, mouse_pos)
+	if path.size() < path_points:
+		print("no transition")
+	
+	path_points = path.size()
+	
+	if path_points < 2: 
+		print("early ret")
+		return
+
+	global_position += global_position.direction_to(path[1]) * speed
+	
+	update()
