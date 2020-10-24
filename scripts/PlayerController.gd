88ca7960101c9ea2dda9bcf65d3795f726c34568
== <==>
2020-10-24 20:43:52 +0100

    Added enemy counting to player and updated enemy scene

--- scripts/PlayerController.gd
+++ scripts/PlayerController.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 
 export (int) var speed = 350
 var velocity = Vector2()
+var enemies = []
 
 func get_input():
 	velocity = Vector2()
@@ -16,7 +17,23 @@ func get_input():
 	if Input.is_action_pressed('up'):
 		velocity.y -= 1
 	velocity = velocity.normalized() * speed
+	
+	if Input.is_action_pressed('fear'):
+		fear()
 
 func _physics_process(delta):
 	get_input()
 	velocity = move_and_slide(velocity)
+	print(enemies.size())
+
+func fear():
+	pass
+
+func _on_SpellRadius_body_entered(body):
+	if body.is_in_group("Enemies"):
+		enemies.push_back(body)
+
+
+func _on_SpellRadius_body_exited(body):
+	if body.is_in_group("Enemies"):
+		enemies.remove(enemies.find(body))
